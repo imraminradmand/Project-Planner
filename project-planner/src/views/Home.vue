2517ab23cbb40db37f19e441f5a3e7f16af02609
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-12 14:42:16 -0700

    grabbing data from the json and throwing error setup

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -9,6 +9,17 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch ('https://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
