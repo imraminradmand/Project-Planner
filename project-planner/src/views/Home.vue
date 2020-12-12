bc9c959ff22b6166f391480497ee526bf873577b
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-12 15:03:44 -0700

    added onclick for projets

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -2,18 +2,20 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <p>{{project.title}}</p>
+        <SingleProject :project="project"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject'
 export default {
   name: 'Home',
   components: {
-    
+    SingleProject
   },
+
   data() {
     return {
       projects: []
